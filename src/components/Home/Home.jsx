d6a9e9fa64867593a05aedb61129bccf0c519e92
Tiwari <sanjeet.tiwari@siemens.com>
2019-06-13 00:42:17 +0530

    Added the layout for home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,56 @@
 import React, { Component } from 'react';
 import './Home.css';
+import { Templates } from '../../utilities/constants';
+
 export class HomeComponent extends Component {
     state = {
-        x : 23,
-        toChange: 0
+        money: ''
     }
     
     render() {
         return (<div className="container">
-            Value is {this.state.x}
-            <br />
-            <input type="number" onChange={(event) => this.changeSecondVariable(event)}/>
-            &nbsp;
-            <button onClick={this.changeValue}>Click To Change</button>
+            <div className="calculator">
+                <div className="moneyplace">
+                    200.00
+                </div>
+                <div className="moneylabel">
+                    <span>Add a label</span>
+                    <span>
+                        <i className="fas fa-caret-right"></i>
+                        <i className="fas fa-caret-right"></i>
+                        <i className="fas fa-caret-right"></i>
+                    </span>
+                    <select className="moneylabeloption">
+                        {Templates.map(template => {
+                            return (<option>{template.title}</option>)
+                        })}
+                    </select>
+                </div>
+                <hr />
+                <div className="keys">
+                    <div className="powerkey" onClick={this.changeMoney('1')}>1</div>
+                    <div className="powerkey" onClick={this.changeMoney('2')}>2</div>
+                    <div className="powerkey" onClick={this.changeMoney('3')}>3</div>
+                    <div className="powerkey" onClick={this.changeMoney('4')}>4</div>
+                    <div className="powerkey" onClick={this.changeMoney('5')}>5</div>
+                    <div className="powerkey" onClick={this.changeMoney('6')}>6</div>
+                    <div className="powerkey" onClick={this.changeMoney('7')}>7</div>
+                    <div className="powerkey" onClick={this.changeMoney('8')}>8</div>
+                    <div className="powerkey" onClick={this.changeMoney('9')}>9</div>
+                    <div className="powerkey" onClick={this.changeMoney('10')}>#</div>
+                    <div className="powerkey" onClick={this.changeMoney('0')}>0</div>
+                    <div className="powerkey" onClick={this.changeMoney('-1')}>DEL</div>
+                </div>
+            </div>
         </div>)
     }
 
-    changeValue = () => {
-        this.setState({x:this.state.toChange})
-    }
-
-    changeSecondVariable = (evt) => {
-        var y = evt.target.value;
-        this.setState({ toChange: y }, () => {
-            console.log(this.state.toChange);
-        });
+    changeMoney = (keyPressed) => {
+        if (keyPressed === '-1') {
+            this.setState({ money: this.state.money.substring(0, this.state.money.length - 2) });
+        }
+        else {
+            this.setState({ money: `${this.state.money}${keyPressed}` });
+        }
     }
 }
---
+++ src/utilities/constants.js
@@ -0,0 +1,26 @@
+export const Templates = [
+    {
+        title: 'None',
+        code: 'NaN'
+    },
+    {
+        title: 'Food',
+        code: 'f'
+    },
+    {
+        title: 'Travel',
+        code: 't'
+    },
+    {
+        title: 'Shopping',
+        code: 's'
+    },
+    {
+        title: 'Hang Outs',
+        code: 'ho'
+    },
+    {
+        title: 'Rent',
+        code: 'r'
+    }
+]
