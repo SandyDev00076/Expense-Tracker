8e6732fd1a8b15faac6e8e80189037872d523e95
Tiwari <sanjeet.tiwari@siemens.com>
2019-06-14 01:36:00 +0530

    Fixed the bug and added feature buttons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,7 +11,7 @@ export class HomeComponent extends Component {
         return (<div className="container">
             <div className="calculator">
                 <div className="moneyplace">
-                    200.00
+                    {(this.state.money.length === 0) ? "What's the expense?" : this.state.money}
                 </div>
                 <div className="moneylabel">
                     <span>Add a label</span>
@@ -28,29 +28,49 @@ export class HomeComponent extends Component {
                 </div>
                 <hr />
                 <div className="keys">
-                    <div className="powerkey" onClick={this.changeMoney('1')}>1</div>
-                    <div className="powerkey" onClick={this.changeMoney('2')}>2</div>
-                    <div className="powerkey" onClick={this.changeMoney('3')}>3</div>
-                    <div className="powerkey" onClick={this.changeMoney('4')}>4</div>
-                    <div className="powerkey" onClick={this.changeMoney('5')}>5</div>
-                    <div className="powerkey" onClick={this.changeMoney('6')}>6</div>
-                    <div className="powerkey" onClick={this.changeMoney('7')}>7</div>
-                    <div className="powerkey" onClick={this.changeMoney('8')}>8</div>
-                    <div className="powerkey" onClick={this.changeMoney('9')}>9</div>
-                    <div className="powerkey" onClick={this.changeMoney('10')}>#</div>
-                    <div className="powerkey" onClick={this.changeMoney('0')}>0</div>
-                    <div className="powerkey" onClick={this.changeMoney('-1')}>DEL</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('1')}>1</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('2')}>2</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('3')}>3</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('4')}>4</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('5')}>5</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('6')}>6</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('7')}>7</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('8')}>8</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('9')}>9</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('00')}>00</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('0')}>0</div>
+                    <div className="powerkey" onClick={() => this.changeMoney('-1')}>DEL</div>
+                </div>
+                <hr />
+                <div className="taskbar">
+                    <div className="taskbtn" id="clear" onClick={() => this.changeMoney('#')}>
+                        Clear
+                    </div>
+                    <div className="taskbtn" id="record" onClick={this.recordExpense}>
+                        Record
+                    </div>
                 </div>
             </div>
         </div>)
     }
 
     changeMoney = (keyPressed) => {
-        if (keyPressed === '-1') {
-            this.setState({ money: this.state.money.substring(0, this.state.money.length - 2) });
+        switch(keyPressed) {
+            case '-1': {
+                if (this.state.money.length !== 0) this.setState({ money: this.state.money.substring(0, this.state.money.length - 1) });
+            }
+            break;
+            case '#': {
+                this.setState({ money: '' });
+            }
+            break;
+            default: {
+                if ((keyPressed !== '0' && keyPressed !== '00') || this.state.money.length !== 0) this.setState({ money: `${this.state.money}${keyPressed}` });
+            }
         }
-        else {
-            this.setState({ money: `${this.state.money}${keyPressed}` });
-        }
+    }
+
+    recordExpense = () => {
+        console.log(this.state.money);
     }
 }
